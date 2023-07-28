40f67c7d738bc561c8384bde3fba0683c70e6354
saguser <prakashb2252002@gmail.com>
2023-07-28 18:44:11 +0530

    first commit

---
+++ src/components/banner/Media.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { FaLinkedinIn, FaReact , FaGithub } from "react-icons/fa";
+import { SiTailwindcss, SiNodedotjs, SiExpress, SiMongodb, SiHtml5, SiCss3, SiJavascript } from "react-icons/si";
+const Media = () => {
+  return (
+    <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            Find me in
+          </h2>
+          <div className="flex gap-4">
+            
+            <span className="bannerIcon">
+              <FaLinkedinIn />
+            </span>
+            <span className="bannerIcon">
+                    <FaGithub/>
+                  </span>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            BEST SKILL ON
+          </h2>
+          <div className="flex gap-4">
+          <span className="bannerIcon">
+              <SiHtml5/>
+            </span>
+            <span className="bannerIcon">
+              <SiCss3 />
+            </span>
+            <span className="bannerIcon">
+              <SiJavascript />
+            </span>
+            <span className="bannerIcon">
+              <FaReact />
+            </span>
+            <span className="bannerIcon">
+              <SiNodedotjs />
+            </span>
+          
+            <span className="bannerIcon">
+              <SiExpress />
+            </span>
+            <span className="bannerIcon">
+              <SiMongodb />
+            </span>
+            <span className="bannerIcon">
+              <SiTailwindcss />
+            </span>
+            
+            
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Media
